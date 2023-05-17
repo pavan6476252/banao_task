27bc9322a6bc6ed4da76e6354f7af10f81110235
pavan6476252 <meesalapavan2003@gmail.com>
2023-05-17 20:48:03 +0530

    finish all ✅

--- banao_task1/lib/utils/utils.dart
+++ banao_task1/lib/utils/utils.dart
@@ -1,10 +1,6 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 SvgPicture svg(String path) {
   return SvgPicture.asset(path);
 }
 
-showImage() {
-  return CachedNetworkImage(imageUrl: "https://picsum.photos/seed/picsum/242/140");
-}
